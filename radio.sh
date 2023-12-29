bcfedc694e6bc9d30f0ad2d5dc04e645bdf0d1e5
Mauro Novellas <mauro@novellas.es>
2023-12-29 11:04:35 +0100

    Update radio.sh

--- radio.sh
+++ radio.sh
@@ -1,14 +1,65 @@
 #!/bin/bash
 # Script para reproducir estaciones de radio en línea usando cvlc
 
-# Verificar si cvlc está instalado
-if ! command -v cvlc &> /dev/null; then
-    echo "cvlc no está instalado. Intentando instalarlo..."
-    sudo apt-get update
-    sudo apt-get install -y vlc
-fi
+# Función para verificar e instalar cvlc si es necesario
+verificar_e_instalar_cvlc() {
+    if ! command -v cvlc &> /dev/null; then
+        echo "cvlc no está instalado. Intentando instalarlo..."
+        sudo apt-get update
+        sudo apt-get install -y vlc
+        if ! command -v cvlc &> /dev/null; then
+            echo "Error: No se pudo instalar cvlc. Por favor, instálelo manualmente."
+            exit 1
+        fi
+    fi
+}
 
-# URLs y nombres de las radios
+# Función para mostrar el menú de estaciones de radio
+mostrar_menu() {
+    clear  # Limpia la terminal antes de mostrar el menú
+    echo "-== Radios ==-"
+    for i in "${!RADIO_NOMBRES[@]}"; do
+        echo "$((i + 1))) ${RADIO_NOMBRES[$i]}"
+    done
+    echo "Presione '+' para subir el volumen, '-' para bajarlo, 'q' para salir, 'm' para volver al menú."
+}
+
+# Función para ejecutar la estación de radio seleccionada
+ejecutar_radio() {
+    cvlc "${RADIO_URLS[$1]}" &
+    PID_CVLC=$!
+}
+
+# Función para controlar el volumen, salida y volver al menú
+control_teclado() {
+    while true; do
+        read -rsn1 input
+        case "$input" in
+            '+') pactl set-sink-volume @DEFAULT_SINK@ +10% ;;
+            '-') pactl set-sink-volume @DEFAULT_SINK@ -10% ;;
+            'q') 
+                kill $PID_CVLC
+                break ;;
+            'm') 
+                kill $PID_CVLC
+                return 1 ;; # Volver al menú principal
+        esac
+    done
+}
+
+# Función para validar la selección del usuario
+validar_seleccion() {
+    if [[ $1 =~ ^[1-${#RADIO_NOMBRES[@]}]$ ]]; then
+        ejecutar_radio $((estacion-1))
+        if control_teclado; then
+            return 1
+        fi
+    else
+        echo "Opción no válida. Por favor, seleccione un número entre 1 y ${#RADIO_NOMBRES[@]}."
+    fi
+}
+
+# Inicializar URLs y nombres de las radios
 RADIO_URLS=(
 "https://livefastly-webs.ondacero.es/barcelona-pull/audio/chunklist.m3u8"
 "https://25653.live.streamtheworld.com/CADENASER.mp3"
@@ -19,7 +70,6 @@ RADIO_URLS=(
 "https://directes-radio-int.ccma.cat/live-content/catalunya-radio-hls/master.m3u8"
 "https://rockfm-cope.flumotion.com/playlist.m3u8"
 )
-            
 RADIO_NOMBRES=(
 "Onda Cero Barcelona"
 "SER Barcelona"
@@ -31,47 +81,18 @@ RADIO_NOMBRES=(
 "Rock FM"
 )
 
-
-# Mostrar menú
-mostrar_menu() {
-    echo "-== Radios ==-"
-    for i in "${!RADIO_NOMBRES[@]}"; do
-        echo "$((i + 1))) ${RADIO_NOMBRES[$i]}"
-    done
-    echo "Presione '+' para subir el volumen, '-' para bajarlo y 'q' para salir."
-}
-
-# Ejecutar radio
-ejecutar_radio() {
-    cvlc "${RADIO_URLS[$1]}" &
-    PID_CVLC=$!
-}
-
-# Control de volumen y salida
-control_teclado() {
-    while true; do
-        read -rsn1 input
-        if [ "$input" = '+' ]; then
-            pactl set-sink-volume @DEFAULT_SINK@ +10%
-        elif [ "$input" = '-' ]; then
-            pactl set-sink-volume @DEFAULT_SINK@ -10%
-        elif [ "$input" = 'q' ]; then
-            kill -9 $PID_CVLC
-            break
-        fi
-    done
-}
+# Verificar e instalar cvlc
+verificar_e_instalar_cvlc
 
 # Manejar la interrupción del script (Ctrl+C)
-trap 'kill -9 $PID_CVLC; exit' SIGINT
+trap 'kill $PID_CVLC; exit' SIGINT
 
-# Menú principal
-mostrar_menu
-read -p "Seleccione una estación: " estacion
-
-if [[ $estacion =~ ^[1-${#RADIO_NOMBRES[@]}]$ ]]; then
-    ejecutar_radio $((estacion-1))
-    control_teclado
-else
-    echo "Opción no válida."
-fi
+# Bucle del menú principal
+while true; do
+    mostrar_menu
+    read -p "Seleccione una estación (o 'q' para salir): " estacion
+    if [[ "$estacion" == "q" ]]; then
+        break
+    fi
+    validar_seleccion $estacion
+done
